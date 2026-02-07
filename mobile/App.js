ba623259a6534243789c3395ac35416c465a6556
yasin-yp <yasinpopatiya.yp@gmail.com>
2026-02-07 13:44:52 +0200

    Create main App component in mobile/App.js

---
+++ mobile/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Welcome to Agrico Marketing App</h1>
+            {/* Other components will go here */}
+        </div>
+    );
+};
+
+export default App;
